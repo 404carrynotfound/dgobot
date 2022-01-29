64ad6b04aec809ba2ff4ce4dee007b5aca89f1c5
Kristiyan <37977687+404carrynotfound@users.noreply.github.com>
2022-01-29 22:00:13 +0200

    Update player

--- player/player.go
+++ player/player.go
@@ -1,24 +1,26 @@
-package playermanager
+package player
 
 import (
 	"github.com/DisgoOrg/disgolink/lavalink"
 	"sync"
 )
 
-type PlayerManager struct {
+type Manager struct {
 	lavalink.PlayerEventAdapter
 	Player  lavalink.Player
 	Queue   []lavalink.AudioTrack
 	QueueMu sync.Mutex
+	playing bool
+	paused  bool
 }
 
-func (m *PlayerManager) AddQueue(tracks ...lavalink.AudioTrack) {
+func (m *Manager) AddQueue(tracks ...lavalink.AudioTrack) {
 	m.QueueMu.Lock()
 	defer m.QueueMu.Unlock()
 	m.Queue = append(m.Queue, tracks...)
 }
 
-func (m *PlayerManager) PopQueue() lavalink.AudioTrack {
+func (m *Manager) PopQueue() lavalink.AudioTrack {
 	m.QueueMu.Lock()
 	defer m.QueueMu.Unlock()
 	if len(m.Queue) == 0 {
@@ -29,12 +31,21 @@ func (m *PlayerManager) PopQueue() lavalink.AudioTrack {
 	return track
 }
 
-func (m *PlayerManager) OnTrackEnd(player lavalink.Player, _ lavalink.AudioTrack, endReason lavalink.AudioTrackEndReason) {
+func (m *Manager) EmptyQueue() {
+	m.QueueMu.Lock()
+	defer m.QueueMu.Unlock()
+	m.Queue = nil
+}
+
+func (m *Manager) OnTrackEnd(player lavalink.Player, _ lavalink.AudioTrack, endReason lavalink.AudioTrackEndReason) {
 	if !endReason.MayStartNext() {
+		m.playing = false
 		return
 	}
 
 	if track := m.PopQueue(); track != nil {
 		_ = player.Play(track)
+		return
 	}
+	m.playing = false
 }
