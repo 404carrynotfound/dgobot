a940cde55c1c3163806d85a4ca5e40aa8fb690e5
Kristiyan <37977687+404carrynotfound@users.noreply.github.com>
2022-01-31 23:27:41 +0200

    Add test for commands

---
+++ commands_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+// Checks if for every commands there's a function to handle that
+func TestCommands(t *testing.T) {
+	for _, command := range commands {
+		if commandHandlers[command.Name] == nil {
+			t.Errorf("Declared command %s in application command slice, but there's no handler.", command.Name)
+		}
+	}
+}
